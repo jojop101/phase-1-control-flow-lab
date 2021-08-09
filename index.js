183f23fa5aabef274195c739b58a24cb5dd88cee
GiuseppePennisi <jojop101@gmail.com>
2021-08-09 13:38:53 -0400

    commit tree 45517a7037e9e8835bfebe0a89592a83678b7860

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+ 	
+ 	if (num <= 400){
+ 		return ('This one is on me!') //
+ 	}
+ 	else if (num > 2500) {
+ 		return ('No can do.')
+ 	}
+ 	else if (num > 2000){
+ 		return ('I will gladly take your thirty bucks.')
+ 	}
+ 		
+ }
+function ternaryCheckCity(city){
+		return city ==='NYC'?'Ok, sounds good.' : 'No go.'
+		
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+	switch(tip){
+		case 'generous':
+			return 'Thank you so much.'
+		case 'not as generous':
+			return 'Thank you.'
+		default:
+			return 'Bye.'
+	}
   // Write your code here!
 }
